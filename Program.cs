644ddee196a901e0777baaaca24fef027388ea5f
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2022-12-06 19:43:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿class Program
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome Employee");
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(0,2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+
+        }
     }
 }
