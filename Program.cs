70bb97383d5230b9a6b5e6a69e8d1a4aea84a249
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2022-12-06 19:41:00 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
-﻿class Program
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome Employee");
+        int isPartTime = 1;
+        int isFullTime = 2;
+        int empRatePerHour = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 3);
+        if (empCheck == isPartTime) 
+        {
+            empHrs = 4;
+
+        }
+        else if (empCheck == isFullTime)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * empRatePerHour;
+        Console.WriteLine("Emp Wage:----" + empWage);
     }
 }
