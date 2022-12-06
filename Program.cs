e81c8c59e772d2342c9bd53d176647ea03e473a6
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2022-12-06 19:52:55 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,40 @@
-﻿class Program
+ class Program
 {
+    public const int IsPartTime = 1;
+    public const int IsFullTime = 2;
+    public const int EmpRatePerHour = 20;
+    public const int NumOfWorkingDays = 2;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome Employee");
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+
+         for (int day =0; day < NumOfWorkingDays; day++)
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IsPartTime:
+                    empHrs = 4;
+                    break;
+                case IsFullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+
+            }
+            empWage = empHrs * EmpRatePerHour;
+            totalEmpWage += empWage;
+            Console.WriteLine("EmpWage:" + empWage);
+
+        }
+        Console.WriteLine("Total Emp Wage :" + totalEmpWage);
     }
 }
