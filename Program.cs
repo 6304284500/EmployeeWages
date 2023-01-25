bbfb3051ad8a273c896b01f266d21ab5904dcac9
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-25 09:21:30 +0530

    [Sreeram]-UC7_UsingClassMethods

--- Program.cs
+++ Program.cs
@@ -1,8 +1,40 @@
-﻿class Program
+﻿public class Program
 {
+    public const int IS_PART_Time = 1;
+    public const int IS_FULL_Time = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WorkingDays = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
+    public static int computeEmpWage()
+    {
+        //Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        //Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WorkingDays ) 
+        {
+            totalWorkingDays++;
+            Random random= new Random();
+            int empCheck = random.Next(0,3);
+            switch ( empCheck )
+            {
+                case IS_PART_Time:
+                    empHrs =4;
+                    break;
+                case IS_FULL_Time:
+                    empHrs =8;
+                    break;
+                default: empHrs = 0; break;
+
+            }
+            totalEmpHrs += empHrs ;
+            Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+        }
+        int totalEmWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Total Emp Wage:" + totalEmWage);
+        return totalEmWage;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome Employee");
+        computeEmpWage();    
     }
 }
